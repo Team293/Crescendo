3cbad6f1071921ea7a1f5f87aba1bfe737afbb82
Brennan McTernan <bjmcternan@gmail.com>
2024-01-10 19:43:13 -0500

    Totally blank project

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Crescendo";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 513;
+  public static final String GIT_SHA = "860ed4c703e2b38e2e680d66cd377527b417252e";
+  public static final String GIT_DATE = "2024-01-08 15:31:02 EST";
+  public static final String GIT_BRANCH = "BlankProject";
+  public static final String BUILD_DATE = "2024-01-10 19:35:53 EST";
+  public static final long BUILD_UNIX_TIME = 1704933353776L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
