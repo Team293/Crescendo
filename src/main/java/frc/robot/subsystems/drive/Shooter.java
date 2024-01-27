08d7059ee15cb6d65802983c9dbf6bd471d90b9e
stormDragon777 <tyhriordan1@gmail.com>
2024-01-27 13:09:06 -0500

    Added "hasNote" method to Smart Dashboard

--- src/main/java/frc/robot/subsystems/drive/Shooter.java
+++ src/main/java/frc/robot/subsystems/drive/Shooter.java
@@ -1,11 +1,18 @@
 package frc.robot.subsystems.drive;
 
 import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   private ColorSensorV3 m_colorSensor;
+
+  @Override
+  public void periodic(){
+    SmartDashboard.putBoolean("Note detected", hasNote());
+  }
 
   public boolean hasNote() {
     Color noteColor = m_colorSensor.getColor();
