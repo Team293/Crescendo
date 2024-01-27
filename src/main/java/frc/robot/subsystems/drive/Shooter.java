f6422c641a30b35ca1baa82ce3fa962b5651b22f
PranjalMhetre <techstarpm@gmail.com>
2024-01-27 14:36:11 -0500

    space

--- src/main/java/frc/robot/subsystems/drive/Shooter.java
+++ src/main/java/frc/robot/subsystems/drive/Shooter.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.drive;
-
 import com.revrobotics.ColorSensorV3;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,11 +8,11 @@ public class Shooter extends SubsystemBase {
   private ColorSensorV3 m_colorSensor;
 
   @Override
-  public void periodic(){
+  public void periodic() {
     SmartDashboard.putBoolean("Note detected", hasNote());
     SmartDashboard.putNumber("Distance", m_colorSensor.getProximity());
   }
-  
+
   public boolean hasNote() {
     Color noteColor = m_colorSensor.getColor();
     return noteColor == Color.kOrange;
