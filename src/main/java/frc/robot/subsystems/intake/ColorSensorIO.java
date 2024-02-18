c26e83bc7901b841c70352150ebc897cebcbf6e6
bai cai <86130439+LimeLlama21@users.noreply.github.com>
2024-02-18 14:51:17 -0500

    Moved color sensor files from launcher folder to intake folder

--- src/main/java/frc/robot/subsystems/intake/ColorSensorIO.java
+++ src/main/java/frc/robot/subsystems/intake/ColorSensorIO.java
@@ -11,7 +11,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 
-package frc.robot.subsystems.launcher;
+package frc.robot.subsystems.intake;
 
 import org.littletonrobotics.junction.AutoLog;
 
