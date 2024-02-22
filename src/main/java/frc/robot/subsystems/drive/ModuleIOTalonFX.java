7b179f1bc6ee8ade0b3f3c2ba97a789f50a100c2
Trevor <65660736+Paridax@users.noreply.github.com>
2024-02-21 21:29:21 -0500

    Remove unnecessary getSelectedSensorVelocity

--- src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
@@ -231,8 +231,4 @@ public class ModuleIOTalonFX implements ModuleIO {
     config.NeutralMode = enable ? NeutralModeValue.Brake : NeutralModeValue.Coast;
     turnTalon.getConfigurator().apply(config);
   }
-
-  public double getSelectedSensorVelocity() {
-    return driveTalon.getVelocity().getValueAsDouble();
-  }
 }
