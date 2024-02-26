b465bdeaca3a19e1a309a50752b4dec68db17ae6
stormDragon777 <tyhriordan1@gmail.com>
2024-02-26 17:04:29 -0500

    Deleted rainbow function in ActivateLEDs. java

--- src/main/java/frc/robot/commands/ActivateLEDs.java
+++ src/main/java/frc/robot/commands/ActivateLEDs.java
@@ -51,18 +51,4 @@ public class ActivateLEDs extends Command {
   public boolean isFinished(){
     return true;
   }
-  private void rainbow() {
-    // For every pixel
-    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
-      // Calculate the hue - hue is easier for rainbows because the color
-      // shape is a circle so only one value needs to precess
-      final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
-      // Set the value
-      m_ledBuffer.setHSV(i, hue, 255, 128);
-    }
-    // Increase by to make the rainbow "move"
-    m_rainbowFirstPixelHue += 3;
-    // Check bounds
-    m_rainbowFirstPixelHue %= 180;
-  }
 }
